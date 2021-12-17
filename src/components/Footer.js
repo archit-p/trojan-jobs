7406cd86142d6251dc6051d6d0fe416c4634e723
Archit Pandey <archpndy@gmail.com>
2021-12-17 01:25:13 -0800

    Update email address
    Signed-off-by: Archit Pandey <archpndy@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function Footer() {
+  let email = "architpa@usc.edu";
   return (
     <footer className="py-3 text-sm text-gray-600 font-light">
       Want to post a job? Email{" "}
-      <a className="underline" href="mailto:contact@trojanjobs.xyz">
-        architpa@usc.edu
+      <a className="underline" href={`mailto:${email}`}>
+        {email}
       </a>
     </footer>
   );
