e8ea3bef098c8627910f12557b11b8dc04a4f25c
Archit Pandey <archpndy@gmail.com>
2021-12-19 23:23:38 -0800

    Add header to posting page
    Signed-off-by: Archit Pandey <archpndy@gmail.com>

--- src/templates/JobPosting.js
+++ src/templates/JobPosting.js
@@ -2,33 +2,47 @@ import React from "react";
 import { graphql } from "gatsby";
 import Ruler from "../components/Ruler";
 import Footer from "../components/Footer";
+import { Helmet } from "react-helmet";
 
 function JobPosting({ data: { markdownRemark: context } }) {
   return (
-    <div className="max-w-2xl mx-auto px-2">
-      <article className="py-3">
-        <h2>{context.frontmatter.title}</h2>
-        <div role="doc-subtitle" className="text-gray-600 font-light">
-          {context.frontmatter.employer}
-        </div>
-        <footer className="text-gray-600 text-sm capitalize mt-2 font-light">
-          <time>{context.frontmatter.postedAt}</time>
-          {context.frontmatter.tags.map((tag) => (
-            <>
-              <span className="ml-2">•</span>
-              <span className="ml-2">{tag}</span>
-            </>
-          ))}
-        </footer>
-      </article>
-      <Ruler />
-      <div
-        className="prose py-3"
-        dangerouslySetInnerHTML={{ __html: context.html }}
-      ></div>
-      <Ruler />
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+        <title>
+          {context.frontmatter.title} at {context.frontmatter.employer}
+        </title>
+      </Helmet>
+      <div className="max-w-2xl mx-auto px-2">
+        <header className="py-3">
+          <h1 className="text-lg font-semibold">Trojan Jobs 👩‍💻👨‍💼👩‍🔬</h1>
+          <div role="doc-subtitle" className="text-gray-600">
+            Find on-campus jobs at USC
+          </div>
+        </header>
+        <Ruler />
+        <article className="py-3">
+          <h2>{context.frontmatter.title}</h2>
+          <div role="doc-subtitle" className="text-gray-600 font-light">
+            {context.frontmatter.employer}
+          </div>
+          <footer className="text-gray-600 text-sm capitalize mt-2 font-light">
+            <time>{context.frontmatter.postedAt}</time>
+            {context.frontmatter.tags.map((tag) => (
+              <>
+                <span className="ml-2">•</span>
+                <span className="ml-2">{tag}</span>
+              </>
+            ))}
+          </footer>
+        </article>
+        <div
+          className="prose py-3"
+          dangerouslySetInnerHTML={{ __html: context.html }}
+        ></div>
+        <Ruler />
+        <Footer />
+      </div>
+    </>
   );
 }
 
